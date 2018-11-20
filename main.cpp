70467ff2c9247cd34b76fd8f2e6eafd5331f8d10
ReeceBrouwer <brouwerreece@gmail.com>
2018-11-20 09:38:39 -0500

    Added some shit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//Main
+#include std::string;
+string name = "Reece";
+
+player Reece = new player(name, side, );
+Reece.setName()
+
+player();
+player(name, side, money)
+
+
+player(name,side,money)
